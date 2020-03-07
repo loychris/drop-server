62db828b71ff38606f161d792af83fc9c6a8eb42
Christoph Loy <cloy202@gmail.com>
2020-03-07 21:54:46 +0100

    changed name

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
-app.get('/', (req,res) => {
-    res.send('Hello World!');
-})
+// Serve the static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
 
-const server = app.listen(8080, () => {
-    const host = server.address().address;
-    const port = server.address().port;
-
-    console.log(`Example app listening at http://${host}:${port}`);
+// An api endpoint that returns a short list of items
+app.get('/api/getList', (req,res) => {
+    var list = ["item1", "item2", "item3"];
+    res.json(list);
+    console.log('Sent list of items');
 });
+
+// Handles any requests that don't match the ones above
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname+'/dropWebApp/build/index.html'));
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port);
+
+console.log('App is listening on port ' + port);
