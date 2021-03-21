f4dcd5ff5c08faa28baf4e53fd664c24f272f524
Chris Loy <cloy202@gmail.com>
2021-03-21 15:57:07 +0100

    Added port env

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const Drop = require('./models/drop-schema');
 
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
